b97b84e916b1678072dba1296257f8d8a70599ed
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-04-27 21:23:24 +0300

    A create-order page is completely maked

--- frontend/src/app/admin/create-order/create-order.component.ts
+++ frontend/src/app/admin/create-order/create-order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FormValidators } from '../../shared/form.validators'
+import { FormBottom } from '../../shared/classes/form-bt-class';
 
 @Component({
   selector: 'app-create-order',
@@ -10,6 +11,9 @@ import { FormValidators } from '../../shared/form.validators'
 export class CreateOrderComponent implements OnInit {
 
   form: FormGroup
+
+  // Виды услуг для селектора в шаблоне
+  typeofacts: string[] = ["Рафтинг", "Проведение мероприятий", "Туры / Походы", "Аренда площадок", "Аренда байдарок", "Прогулки на каяках", "Другое"];    
 
   constructor() { }
 
@@ -30,12 +34,31 @@ export class CreateOrderComponent implements OnInit {
       order_email: new FormControl(null, [
         Validators.email
       ]),
+      order_typeofact: new FormControl('', Validators.required),
       order_promo: new FormControl(null, [
         Validators.minLength(3),
         Validators.maxLength(30),
         FormValidators.userPromo
       ])   
     })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return  
+    }  
+
+    const order: FormBottom = {      
+      name: this.form.value.order_name.trim(), 
+      phone: this.form.value.order_phone.trim(),
+      email: this.form.value.order_email.trim(),
+      typeofact: this.form.value.order_typeofact, 
+      text: this.form.value.order_text.trim(), 
+      promo: this.form.value.order_promo.trim(),
+      typeofform: 6,
+      status: false
+    }
+
   }
 
 }
