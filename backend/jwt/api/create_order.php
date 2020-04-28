9ef66b85369979181b9ec03ca372beceb3bd2876
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-04-28 23:27:36 +0300

    Added a backend part for the new orders creation

---
+++ backend/jwt/api/create_order.php
@@ -0,0 +1,63 @@
+<?php
+// требуемые заголовки 
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+
+// header("Access-Control-Allow-Origin: http://localhost:4200");
+/*
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+*/
+
+
+// подключение к БД 
+// файлы, необходимые для подключения к базе данных 
+include_once 'config/database.php';
+include_once 'objects/order.php';
+ 
+// получаем соединение с базой данных 
+$database = new Database();
+$db = $database->getConnection();
+ 
+// создание объекта 'Order' 
+$order = new Order($db);
+ 
+// отправляемые данные будут здесь
+
+// получаем данные 
+$data = json_decode(file_get_contents("php://input"));
+ 
+// устанавливаем значения 
+$order->order_form_type = $data->typeofform;
+$order->order_typeofact = $data->typeofact;
+$order->order_name = $data->name;
+$order->order_phone = $data->phone;
+$order->order_email = $data->email;
+$order->order_promo = $data->promo;
+$order->order_text = $data->text;
+ 
+// здесь будет метод create()
+
+// создание нового заказа 
+if ($order->create()) {
+    // устанавливаем код ответа 
+    http_response_code(200);
+ 
+    // покажем сообщение о том, что новый заказ был создан 
+    echo json_encode(array("message" => "Новый заказ был создан."));
+}
+ 
+// сообщение, если не удаётся создать новый заказ 
+else {
+     // устанавливаем код ответа 
+    http_response_code(400);
+ 
+    // покажем сообщение о том, что создать новый заказ не удалось 
+    echo json_encode(array("message" => "Невозможно создать новый заказ."));
+}
+?>
---
+++ backend/jwt/api/objects/order.php
@@ -0,0 +1,168 @@
+<?php
+// объект 'Order' 
+class Order {
+ 
+    // подключение к БД таблице "orders" 
+    private $conn;
+    private $table_name = "orders";
+ 
+    // свойства объекта 
+    public $order_id;
+    public $order_form_type;
+    public $order_typeofact;
+    public $order_name;
+    public $order_phone;
+    public $order_email;
+    public $order_promo;
+    public $order_text;    
+ 
+    // конструктор класса Order 
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Создание нового заказа 
+    function create() {
+    
+        // Вставляем запрос 
+        $query = "INSERT INTO " . $this->table_name . "
+                SET
+                    order_name = :order_name,
+                    order_phone = :order_phone,
+                    order_typeofact = :order_typeofact,
+                    order_email = :order_email,
+                    order_text = :order_text,
+                    order_promo = :order_promo,
+                    order_form_type = :order_form_type                                        
+        ";       
+
+        // подготовка запроса 
+        $stmt = $this->conn->prepare($query);
+    
+        // инъекция 
+        $this->order_name=htmlspecialchars(strip_tags($this->order_name));
+        $this->order_phone=htmlspecialchars(strip_tags($this->order_phone));
+        $this->order_typeofact=htmlspecialchars(strip_tags($this->order_typeofact));
+        $this->order_email=htmlspecialchars(strip_tags($this->order_email));
+        $this->order_text=htmlspecialchars(strip_tags($this->order_text));
+        $this->order_promo=htmlspecialchars(strip_tags($this->order_promo));
+        $this->order_form_type=htmlspecialchars(strip_tags($this->order_form_type));
+        
+        // привязываем значения 
+        $stmt->bindParam(':order_name', $this->order_name);
+        $stmt->bindParam(':order_phone', $this->order_phone);
+        $stmt->bindParam(':order_typeofact', $this->order_typeofact);
+        $stmt->bindParam(':order_email', $this->order_email);
+        $stmt->bindParam(':order_text', $this->order_text);
+        $stmt->bindParam(':order_promo', $this->order_promo);   
+        $stmt->bindParam(':order_form_type', $this->order_form_type);  
+    
+        // Выполняем запрос 
+        // Если выполнение успешно, то информация о пользователе будет сохранена в базе данных 
+        if($stmt->execute()) {
+            return true;
+        }
+    
+        return false;
+    }
+
+/*
+
+    // здесь будет метод emailExists() 
+
+    // Проверка, существует ли электронная почта в нашей базе данных 
+    function emailExists(){
+    
+        // запрос, чтобы проверить, существует ли электронная почта 
+        $query = "SELECT id, firstname, lastname, password
+                FROM " . $this->table_name . "
+                WHERE email = ?
+                LIMIT 0,1";
+    
+        // подготовка запроса 
+        $stmt = $this->conn->prepare( $query );
+    
+        // инъекция 
+        $this->email=htmlspecialchars(strip_tags($this->email));
+    
+        // привязываем значение e-mail 
+        $stmt->bindParam(1, $this->email);
+    
+        // выполняем запрос 
+        $stmt->execute();
+    
+        // получаем количество строк 
+        $num = $stmt->rowCount();
+    
+        // если электронная почта существует, 
+        // присвоим значения свойствам объекта для легкого доступа и использования для php сессий 
+        if($num>0) {
+    
+            // получаем значения 
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+            // присвоим значения свойствам объекта 
+            $this->id = $row['id'];
+            $this->firstname = $row['firstname'];
+            $this->lastname = $row['lastname'];
+            $this->password = $row['password'];
+    
+            // вернём 'true', потому что в базе данных существует электронная почта 
+            return true;
+        }
+    
+        // вернём 'false', если адрес электронной почты не существует в базе данных 
+        return false;
+    }    
+
+    // здесь будет метод update()
+    
+    // обновить запись заказа
+    public function update(){
+    
+        // Если в HTML-форме был введен пароль (необходимо обновить пароль) 
+        $password_set=!empty($this->password) ? ", password = :password" : "";
+    
+        // если не введен пароль - не обновлять пароль 
+        $query = "UPDATE " . $this->table_name . "
+                SET
+                    firstname = :firstname,
+                    lastname = :lastname,
+                    email = :email
+                    {$password_set}
+                WHERE id = :id";
+    
+        // подготовка запроса 
+        $stmt = $this->conn->prepare($query);
+    
+        // инъекция (очистка) 
+        $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+        $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+    
+        // привязываем значения с HTML формы 
+        $stmt->bindParam(':firstname', $this->firstname);
+        $stmt->bindParam(':lastname', $this->lastname);
+        $stmt->bindParam(':email', $this->email);
+    
+        // метод password_hash () для защиты пароля пользователя в базе данных 
+        if(!empty($this->password)){
+            $this->password=htmlspecialchars(strip_tags($this->password));
+            $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+            $stmt->bindParam(':password', $password_hash);
+        }
+    
+        // уникальный идентификатор записи для редактирования 
+        $stmt->bindParam(':id', $this->id);
+    
+        // Если выполнение успешно, то информация о пользователе будет сохранена в базе данных 
+        if($stmt->execute()) {
+            return true;
+        }
+    
+        return false;
+    }
+
+    */
+
+}
