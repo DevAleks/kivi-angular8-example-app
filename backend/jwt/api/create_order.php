83f0c3732643ba0fa00cb369d9d08e30fb30f758
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-05-07 22:04:45 +0300

    Added a token check before sending the order creation data to DB

--- backend/jwt/api/create_order.php
+++ backend/jwt/api/create_order.php
@@ -13,61 +13,111 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 // Переменная для кода ответа сервер
 $httpResponseCode = 500;
 
-// Переменная для текста ответа сервера
-$answer = '{Message: "Unknown error"}';
+// Переменная для текста ответа сервера если сломалось что-то неизвестное
+$answer = '{"message": "Unknown error"}';
 
 // Показ ошибок в ответе сервера (включено если 1)
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
-// подключение к БД 
-// файлы, необходимые для подключения к базе данных 
-include_once 'config/database.php';
-include_once 'objects/order.php';
+// требуется для кодирования веб-токена JSON 
+include_once 'config/core.php';
+include_once '../libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../libs/php-jwt-master/src/ExpiredException.php';
+include_once '../libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
 
-// получаем соединение с базой данных 
-$database = new Database();
-$db = $database->getConnection();
- 
-// создание объекта 'Order' 
-$order = new Order($db);
- 
-// получаем данные 
-$data = json_decode(file_get_contents("php://input"));
+// получаем jwt 
+$jwt = isset($_GET['auth']) ? $_GET['auth'] : "";
 
-// устанавливаем значения 
-$order->order_form_type = $data->typeofform;
-$order->order_typeofact = $data->typeofact;
-$order->order_name = $data->name;
-$order->order_phone = $data->phone;
-$order->order_email = $data->email;
-$order->order_promo = $data->promo;
-$order->order_text = $data->text;
-
-// создание нового заказа 
-if (
-    !empty($order->order_name) &&
-    !empty($order->order_phone) &&
-    !empty($order->order_typeofact) &&
-    !empty($order->order_text) &&
-    $order->createOrder()
-) {
-    // устанавливаем код ответа 
-    $httpResponseCode = 200;
+// если JWT не пуст 
+if($jwt) {
  
-    // покажем сообщение о том, что новый заказ был создан 
-    $answer = json_encode(array("message" => "A new order was successfully created"));
+    try {
+
+        // декодирование jwt 
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+
+        // если декодирование выполнено успешно, и метод try не выдал Exception, 
+        // то подключаемся к БД, получаем данные из JSON с фронтенда 
+        // и пытаемся эти данные внести в БД
+
+        // файлы, необходимые для подключения к базе данных 
+        include_once 'config/database.php';
+        include_once 'objects/order.php';
+
+        // получаем соединение с базой данных 
+        $database = new Database();
+        $db = $database->getConnection();
+        
+        // создание объекта 'Order' 
+        $order = new Order($db);
+        
+        // получаем данные 
+        $data = json_decode(file_get_contents("php://input"));
+
+        // устанавливаем значения для записи в БД
+        $order->order_form_type = $data->typeofform;
+        $order->order_typeofact = $data->typeofact;
+        $order->order_name = $data->name;
+        $order->order_phone = $data->phone;
+        $order->order_email = $data->email;
+        $order->order_promo = $data->promo;
+        $order->order_text = $data->text;
+
+        // создание нового заказа 
+        if (
+            !empty($order->order_name) &&
+            !empty($order->order_phone) &&
+            !empty($order->order_typeofact) &&
+            !empty($order->order_text) &&
+            $order->createOrder()
+        ) {
+            // устанавливаем код ответа 
+            $httpResponseCode = 200;
+        
+            // покажем сообщение о том, что новый заказ был создан 
+            $answer = json_encode(array(
+                "message" => "A new order was successfully created"
+            ));
+        }
+ 
+        // сообщение, если не удаётся создать новый заказ 
+        else {    
+            // устанавливаем код ответа 
+            $httpResponseCode = 400;
+        
+            // покажем сообщение о том, что создать новый заказ не удалось 
+            $answer = json_encode(array("message" => "Can't create a new order"));
+        }
+
+    }
+
+    // если декодирование не удалось, это означает, что JWT является недействительным 
+    catch (Exception $e){
+    
+        // код ответа 
+        $httpResponseCode = 401;
+    
+        // сообщение об ошибке 
+        $answer = json_encode(array(
+            "message" => "Доступ закрыт, JWT не действителен",
+            "error" => $e->getMessage()
+        ));
+    }    
+
 }
+// показать сообщение об ошибке, если jwt пуст 
+else {
  
-// сообщение, если не удаётся создать новый заказ 
-else {    
-    // устанавливаем код ответа 
-    $httpResponseCode = 400;
+    // код ответа 
+    $httpResponseCode = 401;
  
-    // покажем сообщение о том, что создать новый заказ не удалось 
-    $answer = json_encode(array("message" => "Can't create a new order"));
-}
+    // сообщить пользователю что доступ запрещен 
+    $answer = json_encode(array("message" => "Доступ закрыт, JWT пуст"));
+} 
 
 http_response_code($httpResponseCode);
 
