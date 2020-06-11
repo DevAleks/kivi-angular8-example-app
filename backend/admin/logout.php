8816ff20f974d69704e7e7c84647ea4d18d68a2f
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-06-11 14:27:42 +0300

    Removed an unused part of backend

--- backend/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-setcookie('hash', '', (time()-10000));
-$_SESSION = array(); 
-session_unset();
-session_write_close();
-header("location:login.php");	
-?> 
--- backend/admin/logpas/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$mysqli = new mysqli("mysql", "root", "root", "kiviapp");
