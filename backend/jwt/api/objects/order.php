287d96e055a5bae03dd60ba753474cba54dbd815
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-06-11 15:46:58 +0300

    Updated the handling of frontend orders on the backend, changed mysqli to PDO

--- backend/jwt/api/objects/order.php
+++ backend/jwt/api/objects/order.php
@@ -21,7 +21,7 @@ class Order {
     public function __construct($db) {
         $this->conn = $db;
     }
-
+  
     // Создание нового заказа для create_order.php
     function createOrder() {
         
@@ -51,7 +51,7 @@ class Order {
         $this->order_email=htmlspecialchars(strip_tags($this->order_email));
         $this->order_promo=htmlspecialchars(strip_tags($this->order_promo));
         $this->order_form_type=htmlspecialchars(strip_tags($this->order_form_type));
-        // из order_text убарно удаление html тегов и преобразование символов
+        // из order_text убрано удаление html тегов и преобразование символов
         //$this->order_text=htmlspecialchars(strip_tags($this->order_text));
         
 
@@ -246,4 +246,61 @@ class Order {
 
         return true;
     }
+
+    // Создание нового заказа для requests.add.php
+    function createOrderFront() {
+        
+        // Вставляем запрос 
+        $query = "INSERT INTO " . $this->table_name . "
+                SET
+                    order_name = :order_name,
+                    order_phone = :order_phone,
+                    order_typeofact = :order_typeofact,
+                    order_email = :order_email,
+                    order_text = :order_text,
+                    order_promo = :order_promo,
+                    order_form_type = :order_form_type";       
+
+        // подготовка запроса в БД и обработка ошибок
+        try {
+            $stmt = $this->conn->prepare($query);
+        } catch (Exception $exception) {
+            error_log("Unable to prepare statement: " . $exception->getMessage());
+            return false;
+        }
+    
+        // инъекция 
+        $this->order_name=htmlspecialchars(strip_tags($this->order_name));
+        $this->order_phone=htmlspecialchars(strip_tags($this->order_phone));
+        $this->order_typeofact=htmlspecialchars(strip_tags($this->order_typeofact));
+        $this->order_email=htmlspecialchars(strip_tags($this->order_email));
+        $this->order_promo=htmlspecialchars(strip_tags($this->order_promo));
+        $this->order_form_type=htmlspecialchars(strip_tags($this->order_form_type));
+        // из order_text убрано удаление html тегов и преобразование символов
+        //$this->order_text=htmlspecialchars(strip_tags($this->order_text));
+        
+
+        // привязываем значения 
+        $stmt->bindParam(':order_name', $this->order_name);
+        $stmt->bindParam(':order_phone', $this->order_phone);
+        $stmt->bindParam(':order_typeofact', $this->order_typeofact);
+        $stmt->bindParam(':order_email', $this->order_email);
+        $stmt->bindParam(':order_text', $this->order_text);
+        $stmt->bindParam(':order_promo', $this->order_promo);   
+        $stmt->bindParam(':order_form_type', $this->order_form_type);  
+    
+        // Выполняем запрос и ловим возможные ошибки
+        // Если выполнение успешно, то информация о новом заказе будет сохранена в базе данных
+        try {
+            $stmt->execute();
+            $stmt = null;
+            $this->conn = null;
+        } catch (Exception $exception) {
+            error_log("Unable to execute statement: " . $exception->getMessage());
+            return false;
+        }
+
+        return true;
+    }    
+
 }
--- backend/requests.add.php
+++ backend/requests.add.php
@@ -3,73 +3,111 @@ header('Access-Control-Allow-Origin: http://localhost:4200');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  return 0;
+}
+
+// Переменная для кода ответа сервер
+$httpResponseCode = 500;
+
+// Переменная для текста ответа сервера если сломалось что-то неизвестное
+$answer = '{"message": "Unknown error"}';
+
+// Показ ошибок в ответе сервера (включено если 1)
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+
 // Получаем данные формы topForm из приложения
 $formbt = json_decode(file_get_contents('php://input'), true);
-   
-if(isset($formbt['phone']) && isset($formbt['typeofact']) && isset($formbt['typeofform']))
-{
-    // Обрабатываем и отправляем обратно в приложение ОБЯЗАТЕЛЬНЫЕ данные из формы 
-    $formbt_back['phone'] = 'Телефон: '.$formbt['phone'];    
-    $formbt_back['typeofact'] = 'Тип активности: '.$formbt['typeofact'];
-    $formbt_back['typeofform'] = $formbt['typeofform']; // Тип формы
-    $formbt_back['status'] = false; // Статус ошибки запроса в БД   
-    
-    // Обрабатываем и отправляем обратно в приложение НЕОБЯЗАТЕЛЬНЫЕ данные из формы,
-    // если они есть
-    if (isset($formbt['name'])) {
-      $formbt_back['name'] = 'Имя: '.$formbt['name']; 
-    } else { 
-      $formbt['name'] = ''; 
-    }
-    
-    if (isset($formbt['email'])) {
-      $formbt_back['email'] = 'Email: '.$formbt['email'];
-    } else {
-      $formbt['email'] = '';
-    }
-    
-    if (isset($formbt['promo'])) {
-      $formbt_back['promo'] = 'Promo: '.$formbt['promo'];
-    } else {
-      $formbt['promo'] = '';
-    }   
 
-    if (isset($formbt['text'])) {
-      $formbt_back['text'] = 'Вопрос: '.$formbt['text']; 
-    } else {
-      $formbt['text'] = '';
-    }      
+if($formbt) {  
 
-    $mysqli = new mysqli("mysql", "root", "root", "kiviapp");
+  try {
 
-    // Обработка ошибки соединения с БД
-    if (mysqli_connect_errno()) {
-      echo json_encode ("Ошибка подключения к БД: %s\n".mysqli_connect_error(), JSON_UNESCAPED_UNICODE);
-    }
+    // файлы, необходимые для подключения к базе данных 
+    include_once 'jwt/api/config/database.php';
+    include_once 'jwt/api/objects/order.php';
+
+    // получаем соединение с базой данных 
+    $database = new Database();
+    $db = $database->getConnection();
+
+    // создание объекта 'Order' 
+    $order = new Order($db);  
     
-    // Запись в БД 
-    $query = "INSERT INTO orders (order_form_type, order_typeofact, order_name, order_phone, order_email, order_promo, order_text) VALUES(
-      '".$formbt['typeofform']."', 
-      '".$formbt['typeofact']."', 
-      '".$formbt['name']."', 
-      '".$formbt['phone']."', 
-      '".$formbt['email']."', 
-      '".$formbt['promo']."', 
-      '".$formbt['text']."');";     
-  
-    // Обработка ошибки запроса в Бд
-    $mysqli->query($query);
-    if ($mysqli->error) {
-      $formbt_back['status'] = true;
+    // устанавливаем значения для записи в БД
+    $order->order_form_type = $formbt['typeofform'];
+    $order->order_typeofact = $formbt['typeofact'];
+    $order->order_phone = $formbt['phone'];
+    $order->order_name = $formbt['name'];
+    $order->order_email = $formbt['email'];
+    $order->order_promo = $formbt['promo'];
+    $order->order_text = $formbt['text'];
+
+    // создание нового заказа c проверкой наличия необходимых значений
+    if (
+      !empty($order->order_phone) &&
+      !empty($order->order_typeofact) &&
+      !empty($order->order_form_type) &&
+      $order->createOrderFront()
+    ) {
+
+      // устанавливаем код ответа 
+      $httpResponseCode = 200;
+
+      // Готовим данные для ответа на фронтенд, если заказ успешно создан 
+      // Обязательные 
+      $formbt_back->phone = 'Телефон: '.$formbt['phone'];    
+      $formbt_back->typeofact = 'Тип активности: '.$formbt['typeofact'];
+      $formbt_back->typeofform = $formbt['typeofform']; // Тип формы
+      $formbt_back->status = false; // Статус ошибки запроса в БД   
+
+      // Необязательные
+      $formbt_back->name = isset($formbt['name']) ? 'Имя: '.$formbt['name'] : '';
+      $formbt_back->email = isset($formbt['email']) ? 'Email: '.$formbt['email'] : '';
+      $formbt_back->promo = isset($formbt['promo']) ? 'Promo: '.$formbt['promo'] : '';
+      $formbt_back->text = isset($formbt['text']) ? 'Вопрос: '.$formbt['text'] : '';
+
+      // Отправляем ответ на фронтенд
+      $answer = json_encode($formbt_back);
     }
-   
-    //Отправка ответа сервера в приложение  
-    echo json_encode($formbt_back);
-   
-    $mysqli->close();
+
+    // сообщение, если не удаётся создать новый заказ 
+    else {    
+      // устанавливаем код ответа 
+      $httpResponseCode = 400;
+
+      // покажем сообщение о том, что создать новый заказ не удалось 
+      $answer = json_encode(array("message" => "Не удалось создать заказ"));
+    }    
+
+  }
+  catch (Exception $e){
+    
+    // код ответа 
+    $httpResponseCode = 400;
+
+    // сообщение об ошибке 
+    $answer = json_encode(array(
+        "message" => "Отправка данных из формы не удалась",
+        "error" => $e->getMessage()
+    ));
+  } 
+
 }
-else
-{  
-    echo "Веденные данные некорректны";
-}
+
+// показать сообщение об ошибке, если данных из формы нет
+else {
+ 
+  // код ответа 
+  $httpResponseCode = 400;
+
+  // сообщить пользователю что доступ запрещен 
+  $answer = json_encode(array("message" => "Заказ не создан. Не удалось получить данные из фронтенд."));
+} 
+
+http_response_code($httpResponseCode);
+
+echo $answer;
     
