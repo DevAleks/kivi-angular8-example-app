c18c83c71c2bd9e741a4e3a1a288399a0c18287d
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-12-28 23:15:55 +0300

    Updated video block layout

--- frontend/src/app/shared/components/video-block/video-block.component.ts
+++ frontend/src/app/shared/components/video-block/video-block.component.ts
@@ -10,25 +10,27 @@ import { JsonInt } from '../../interfaces/interfaces'
   styleUrls: ['./video-block.component.css'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class VideoBlockComponent implements OnInit {
-  videourl: any // why any?
-  error: any // why any? - this variable is never used
-  isVideoLoading = false;
+
+  videourl: string // why any?
+  error: string // why any? - this variable is never used
+  isVideoLoading = false
 
   constructor(
     private getjsonService: GetJsonService,
   ) { }
 
   ngOnInit() {
-    this.isVideoLoading = true; // включаем отображение лоадера на время загрузки видео
+    this.isVideoLoading = true // включаем отображение лоадера на время загрузки видео
     // Получаем url видео из JSON файла
     this.getjsonService.getPagesJson()
       .subscribe((jsonObject: JsonInt[]) => {
-        this.videourl = jsonObject["index"][0]["videoUrl"];
-        this.isVideoLoading = false;
+        this.videourl = jsonObject["index"][0]["videoUrl"]
+        this.isVideoLoading = false
       },
-        error => { this.error = error; }
-      );
+        error => { this.error = error }
+      )
   }
 
 }
